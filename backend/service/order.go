3dff0f5e5d34169fed558e080ab7d89df440e11a
nc-77 <291993554@qq.com>
2021-05-24 15:47:15 +0800

    set default value

--- backend/service/order.go
+++ backend/service/order.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"gorm.io/gorm"
+
 	"time"
 )
 
@@ -10,9 +11,9 @@ type Order struct {
 	GoodId    int            ` json:"good_id" form:"good_id"`
 	Number    float32        `json:"number" form:"number"`
 	BuyerId   int            `json:"buyer_id" form:"buyer_id"`
-	TradeDate time.Time      `json:"trade_date" form:"trade_date"`
-	Status    int            `json:"sellerId" form:"sellerId"`
-	Evaluate  int            `json:"evaluate" from:"evaluate"`
+	TradeDate string         `json:"trade_date" form:"trade_date"`
+	Status    int            `json:"sellerId" form:"sellerId" `
+	Evaluate  int            `json:"evaluate" from:"evaluate" gorm:"default:-1"`
 	CreatedAt time.Time      `json:"created_at" `
 	UpdatedAt time.Time      `json:"updated_at" `
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
